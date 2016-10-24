300f535c212a4ff2dcc4770ad5ab5a747007d4c2
Daniel Schmitz <dr1s@users.noreply.github.com>
2016-10-25 00:06:55 +0700

    Improved research functionality
    - Fixed ResearchItem Ids
    - Bot can do basic researching with „auto_research“ favors ship techs
    over everything else for now
    - Researching is done on the first planet, will be improved in the
    future
    - import switch to explizit mention the class

---
+++ ogbot/core/researcher.py
@@ -0,0 +1,45 @@
+from base import BaseBot
+
+from scraping import research, general
+
+
+class ResearcherBot(BaseBot):
+
+    def __init__(self, browser, config, planets):
+        self.research_client = research.Research(browser, config)
+        self.general_client = general.General(browser, config)
+        self.planets = planets
+        super(ResearcherBot, self).__init__(browser, config, planets)
+
+
+    def get_planet_for_research(self, planets=None):
+        if planets is None:
+            planets = self.planets
+
+        #for now the main planet will be used for research
+        return planets[0]
+
+    def get_next_research_item(self, planet):
+        available_research = self.research_client.get_available_research_for_planet(planet)
+        available_research_item = None
+
+        if available_research is not None:
+            available_research_item = available_research[0]
+
+            self.logger.info("Available Research:")
+            for item in available_research:
+                self.logger.info("      " + item.name)
+
+            # Favor ship upgrades
+            for item in available_research:
+                if item.id in [109, 110, 111]:
+                    available_research_item = item
+                    break
+
+        return available_research_item
+
+    def auto_research_next_item(self):
+        planet = self.get_planet_for_research(self.planets)
+        research = self.get_next_research_item(planet)
+        if research is not None:
+            self.research_client.research_item(research, planet)
--- ogbot/scraping/research.py
+++ ogbot/scraping/research.py
@@ -4,44 +4,45 @@ from general import General
 from scraper import Item, ItemAction, Scraper
 from enum import Enum
 
+class ResearchItem(Item): pass
+
 class ResearchTypes(Enum):
-    EnergyTech                      = "113",
-    LaserTech                       = "120",
-    IonTech                         = "121",
-    HyperspaceTech                  = "114",
-    PlasmaTech                      = "122",
-    CombustionDrive                 = "115",
-    ImpulseDrive                    = "117",
-    HyperspaceDrive                 = "118",
-    EspionageTech                   = "106",
-    ComputerTech                    = "108",
-    AstroPhysics                    = "124",
-    IntergalacticResearchNetwork    = "123",
-    GravitonResearch                = "199",
-    WeaponTech                      = "109",
-    ShieldTech                      = "110",
-    ArmourTech                      = "111"
+    EnergyTech                      = 113,
+    LaserTech                       = 120,
+    IonTech                         = 121,
+    HyperspaceTech                  = 114,
+    PlasmaTech                      = 122,
+    CombustionDrive                 = 115,
+    ImpulseDrive                    = 117,
+    HyperspaceDrive                 = 118,
+    EspionageTech                   = 106,
+    ComputerTech                    = 108,
+    AstroPhysics                    = 124,
+    IntergalacticResearchNetwork    = 123,
+    GravitonResearch                = 199,
+    WeaponTech                      = 109,
+    ShieldTech                      = 110,
+    ArmourTech                      = 111
 
 RESEARCH_DATA = {
-    "113": ResearchItem("113", "Energy Technology"),
-    "120": ResearchItem("120", "Laser Technology"),
-    "121": ResearchItem("121", "Ion Technology"),
-    "114": ResearchItem("114", "Hyperspace Technology"),
-    "122": ResearchItem("122", "Plasma Technology"),
-    "115": ResearchItem("115", "Combustion Drive"),
-    "117": ResearchItem("117", "Impulse Drive"),
-    "118": ResearchItem("118", "Hyperspace Drive"),
-    "106": ResearchItem("106", "Espionage Technology"),
-    "108": ResearchItem("108", "Computer Technology"),
-    "124": ResearchItem("124", "Astro Physics"),
-    "123": ResearchItem("123", "Intergalactic Research Network"),
-    "199": ResearchItem("199", "Graviton Research"),
-    "109": ResearchItem("109", "Weapon Technology"),
-    "110": ResearchItem("110", "Shield Technology"),
-    "111": ResearchItem("111", "Armour Technology")
+    "113": ResearchItem(113, "Energy Technology"),
+    "120": ResearchItem(120, "Laser Technology"),
+    "121": ResearchItem(121, "Ion Technology"),
+    "114": ResearchItem(114, "Hyperspace Technology"),
+    "122": ResearchItem(122, "Plasma Technology"),
+    "115": ResearchItem(115, "Combustion Drive"),
+    "117": ResearchItem(117, "Impulse Drive"),
+    "118": ResearchItem(118, "Hyperspace Drive"),
+    "106": ResearchItem(106, "Espionage Technology"),
+    "108": ResearchItem(108, "Computer Technology"),
+    "124": ResearchItem(124, "Astro Physics"),
+    "123": ResearchItem(123, "Intergalactic Research Network"),
+    "199": ResearchItem(199, "Graviton Research"),
+    "109": ResearchItem(109, "Weapon Technology"),
+    "110": ResearchItem(110, "Shield Technology"),
+    "111": ResearchItem(111, "Armour Technology")
 }
 
-class ResearchItem(Item): pass
 
 class ResearchData(object):
     def __init__(self, research, level):
@@ -100,3 +101,72 @@ class Research(Scraper):
             return ItemAction(ResearchItem(research_data.id, research_data.name), level)
         else:
             return None
+
+
+    def get_available_research_for_planet(self, planet):
+        """ Returns the the research on the page of the planetthat has enough resources to be built """
+
+        url = self.url_provider.get_page_url('research', planet)
+        resp = self.open_url(url)
+
+        soup = BeautifulSoup(resp.read(), "lxml")
+        build_images = soup.findAll("a", {"class": "fastBuild tooltip js_hideTipOnMobile"})
+
+        research = []
+
+        for build_image in build_images:
+            parent_block = build_image.parent
+
+            for i in RESEARCH_DATA.keys():
+                details = "details" + i
+                research_item_btn = parent_block.find("a", {"id": details})
+                if research_item_btn is not None:
+                    research_item = self.get_research_data_from_button(research_item_btn)
+                    if research_item is not None:
+                        research.append(research_item.item)
+
+            research_item = None
+            if research_item_btn is not None:
+                research_item = self.get_research_data_from_button(research_item_btn)
+
+
+            if research_item is not None:
+                research.append(research_item.item)
+                self.logger.info(research_item.item)
+
+        return research if research else None
+
+    def research_item(self, research_data, planet):
+        if self.is_in_research_mode(planet):
+            self.logger.info('Planet %s is already in research mode' % planet.name)
+            return
+        else:
+            self.logger.info('Research %s on planet %s' % (research_data.name, planet.name))
+            self.start_research_item(research_data)
+
+    def start_research_item(self, research_data, planet=None):
+
+        if planet is not None:
+            url = self.url_provider.get_page_url('research', planet)
+            self.open_url(url)
+
+        self.create_control("form", "text", "menge", "1")
+        self.create_control("form", "text", "type", str(research_data.id))
+        self.create_control("form", "text", "modus", "1")
+
+        self.logger.info("Submitting form")
+        self.submit_request()
+
+    def is_in_research_mode(self, planet=None):
+        """
+        Check if the planet is in research mode
+        :param planet: planet to check
+        :return: True if the planet is in construction mode, otherwise returns False
+        """
+
+        url = self.url_provider.get_page_url('research', planet)
+        resp = self.open_url(url)
+        soup = BeautifulSoup(resp.read(), "lxml")
+        # if the planet is in construction mode there shoud be a div with the
+        # class construction
+        return soup.find("div", {"class": "construction"}) != None
