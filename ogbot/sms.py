8b2ca910363c1e9aa56e18485a3e26bd0acb091b
yosh778 <yosh778@gmail.com>
2017-05-17 23:01:04 +0200

    Restored Twilio SMS messaging

--- ogbot/sms.py
+++ ogbot/sms.py
@@ -7,8 +7,7 @@ class SMSSender:
         self.config = config
 
     def send_sms(self, message):
-        test=''
-        #if self.config.enable_twilio_messaging:
-        #    twilio_client = TwilioRestClient(self.config.twilio_account_sid, self.config.twilio_account_token)
-        #    twilio_client.messages.create(to=self.config.twilio_to_number, from_=self.config.twilio_from_number,
-        #                                  body=message)
+        if self.config.enable_twilio_messaging:
+           twilio_client = TwilioRestClient(self.config.twilio_account_sid, self.config.twilio_account_token)
+           twilio_client.messages.create(to=self.config.twilio_to_number, from_=self.config.twilio_from_number,
+                                         body=message)
