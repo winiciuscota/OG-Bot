31a0efb7bf7d5cb6609ac2c06d8a5d36f449f156
yosh778 <yosh778@gmail.com>
2017-07-09 17:07:04 +0200

    +

--- ogbot/core/movement.py
+++ ogbot/core/movement.py
@@ -1,5 +1,6 @@
 from base import BaseBot
 from scraping import movement, fleet
+import datetime
 
 
 class MovementBot(BaseBot):
@@ -13,7 +14,7 @@ class MovementBot(BaseBot):
 
     def check_hostile_activity(self):
         fleet_movement = self.movement_scraper.get_fleet_movement()
-        hostile_movements = filter(lambda x: not x.friendly and x.mission in {'attack', 'allianceAttack'}, fleet_movement)
+        hostile_movements = filter(lambda x: not x.friendly and x.mission in {'attack', 'allianceAttack', 'spy', 'destroyStar'}, fleet_movement)
 
         if len(hostile_movements) == 0:
             self.logger.info("There is no hostile activity now")
@@ -21,18 +22,23 @@ class MovementBot(BaseBot):
         targets = {}
 
         for hostile_movement in hostile_movements:
-            self.logger.warning(hostile_movement)
-            self.logger.warning('Incoming attack on planet %s, attempting fleet escape', hostile_movement.destination_name)
-            self.sms_sender.send_sms(hostile_movement)
 
-            cTarget = hostile_movement.destination_coords
-            target = self.get_player_planet_by_coordinates(cTarget)
-            target.safe = False
+            # Any long espionnage could be an attack
+            if x.mission != 'spy' or hostile_movement.countdown_time > datetime.timedelta(minutes=4):
 
-            if target.moon is not None:
-                target.moon.safe = False
+                self.logger.warning(hostile_movement)
+                self.logger.warning('Incoming attack on planet %s, attempting fleet escape', hostile_movement.destination_name)
+                self.sms_sender.send_sms(hostile_movement)
 
-            targets[cTarget + str(hostile_movement.isMoon)] = target
+                cTarget = hostile_movement.destination_coords
+                target = self.get_player_planet_by_coordinates(cTarget)
+                target.safe = False
+
+                if target.moon is not None:
+                    target.moon.safe = False
+
+                targets[cTarget + str(hostile_movement.isMoon)] = target
+
 
         for coords, target in targets.iteritems():
             self.fleet_client.fleet_escape(target)
